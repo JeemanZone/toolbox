37161c1f3e53c2a27f822be343b6ecad6bdd51fb
Tsumian Tsng <llbmelon@gmail.com>
2024-09-09 22:45:51 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,18 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div>Hello react.</div>
+      <div>
+        别生气啦小宝贝，
+        <a
+          href="#"
+          onClick={() => {
+            alert("爱你啦，没有不理你的意思。");
+          }}
+        >
+          点点这个试试
+        </a>
+        。
+      </div>
     </>
   );
 }
